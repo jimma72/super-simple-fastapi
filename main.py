f32fad68aedc3f14e22731377cba55e5c0ce698a
jimma72 <james@ciendos.com>
2025-08-26 19:50:02 +0100

    bug fix

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World"}
 
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"message": f"Hello Item {item_id}"}
